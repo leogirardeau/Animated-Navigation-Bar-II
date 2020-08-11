bc1ceabff5e316e424d250e985b404360136e975
leogirardeau <leogir.pro@gmail.com>
2020-08-11 10:55:22 +0200

    animations + forgot a .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll("nav-links li");
+    const navLinks = document.querySelectorAll(".nav-links li");
 
     burger.addEventListener("click", () => {
 
@@ -11,11 +11,13 @@ const navSlide = () => {
         //Animate Links
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
-                link.style.animation = ``;
+                link.style.animation = "";
             } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
             }
         });
+        //Burger Animation
+        burger.classList.toggle("toggle");
     });
 }
 
