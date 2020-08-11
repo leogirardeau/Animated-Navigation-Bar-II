430d47ef3e2a43615fd6345a4b50cd9c08e421d7
leogirardeau <leogir.pro@gmail.com>
2020-08-11 10:07:52 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+    });
+}
+
+// better to make and function to call functions when they are multiples
+const app = () => {
+    navSlide();
+}
